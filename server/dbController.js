f0b6ba73ac6c69e0be8e649209adb3fbfb1d4fc1
Jacob Eldred <jeldred95@gmail.com>
2021-01-15 22:11:23 -0800

    Added route and methods for retrieving main item summary information

---
+++ server/dbController.js
@@ -0,0 +1,65 @@
+const { db, models } = require('../database');
+
+const ItemVariants = db.model('ItemVariants');
+
+// Get item data
+module.exports.retrieveItem = (itemId) => models.Item.findAll({
+  where: {
+    id: itemId,
+  },
+})
+  .then((itemResults) => itemResults[0].dataValues)
+  .then((itemData) => {
+    const item = itemData;
+    delete item.createdAt;
+    delete item.updatedAt;
+    return ItemVariants.findAll({
+      where: {
+        ItemId: itemId,
+      },
+    })
+      .then((variantResults) => {
+        if (variantResults.length > 0) {
+          const variantIds = variantResults.map((v) => v.VariantId);
+          item.variants = variantIds;
+        }
+        return item.DepartmentId;
+      })
+      .then((departmentId) => {
+        delete item.DepartmentId;
+        return models.Department.findAll({
+          where: {
+            id: departmentId,
+          },
+        });
+      })
+      .then((departmentResults) => {
+        item.Department = departmentResults[0].name;
+        return item;
+      });
+  })
+
+// Returns
+// {
+//   “id”: number,
+//   “name”: string,
+//   “price”: number,
+//   “discount”: string,
+//   “subcategory”: string,
+//   “category”: string,
+//   “department”: string,
+//   “stock”: number,
+//   “variants”: [
+//     “item_id”: number,
+//     “Item_id”: number...
+//   ]
+// }
+
+// Get images
+
+// Returns
+// {
+// “imageUrls”: [urlStrings...]
+// }
+
+// Post to cart
--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const path = require('path');
+const controller = require('./dbController');
 
 const app = express();
 const port = 12345; // "TBD" // Coordinate with team!
 
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.get('/api/:id/summary', (req, res) => {
+  const { id } = req.params;
+  controller.retrieveItem(id)
+    .then((result) => {
+      res.status(200);
+      res.send(result);
+    })
+    .catch(() => {
+      res.sendStatus(404);
+    });
+});
 /*
 GET /api/:id/summary
 no parameters
