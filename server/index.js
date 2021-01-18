7637cf9e0268c1534130c139e8d4001ce52d2113
Jacob Eldred <jeldred95@gmail.com>
2021-01-18 13:53:19 -0800

    Added router POST path for adding to cart

--- server/index.js
+++ server/index.js
@@ -38,6 +38,19 @@ app.get('/api/:id/images', (req, res) => {
 GET /api/:id/images
 -amount: Number (Default: 10)
  */
+app.post('/api/:id/cart', (req, res) => {
+  const { amount } = req.query;
+  const { id } = req.params;
+
+  controller.addToCart(id, amount)
+    .then((result) => {
+      res.status(201);
+      res.send(result);
+    })
+    .catch(() => {
+      res.sendStatus(404);
+    });
+});
 
 /*
 POST /api/:id/cart
