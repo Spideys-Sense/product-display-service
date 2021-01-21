7455974f3463980eb5dce24aa3212b20c6af09ac
Jacob Eldred <jeldred95@gmail.com>
2021-01-20 16:21:07 -0800

    Implemented ImageList component

--- client/src/components/ImageCarousel/ImageList.jsx
+++ client/src/components/ImageCarousel/ImageList.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import ImageListEntry from './ImageListEntry';
 
-const ListContainer = styled.h5`
+const ListContainer = styled.div`
   grid-area: 1 / 1 / 1 / 1;
   margin: 10px;
   bottom: 1px;
   border: 1px green dashed;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
 
-export default function ImageList() {
+const SlidingImageList = styled.div`
+
+`;
+
+const ScrollButton = styled.button`
+  margin: 0 auto;
+  color: ${(props) => ((props.active) ? '#115AB0' : '#D5D5D5')}
+`;
+
+export default function ImageList(props) {
+  const { urls } = props;
+  console.log(urls);
   return (
     <ListContainer>
-      Little pics here
+      <ScrollButton>▲</ScrollButton>
+      <SlidingImageList>
+        {urls.map((url, i) => <ImageListEntry url={url} key={`IMG${i + 1}`} />)}
+      </SlidingImageList>
+      <ScrollButton>▼</ScrollButton>
     </ListContainer>
   );
 }
+
+ImageList.propTypes = {
+  urls: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
