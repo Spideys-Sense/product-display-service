4a7e7621815fb6ab19544790ff04ecb7b9f2ec25
Jacob Eldred <jeldred95@gmail.com>
2021-01-20 16:21:32 -0800

    Implemented BigPicture component

--- client/src/components/ImageCarousel/BigPicture.jsx
+++ client/src/components/ImageCarousel/BigPicture.jsx
@@ -2,17 +2,43 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const BigPictureFrame = styled.h5`
+const BigPictureFrame = styled.div`
   grid-area: 1 / 2 / 1 / 2;
   margin: 10px;
   bottom: 1px;
   border: 1px green dashed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-export default function BigPicture() {
-  return (
-    <BigPictureFrame>
-      Big pic here
-    </BigPictureFrame>
-  );
+const Img = styled.img`
+  max-height: 100%;
+  max-width: 100%;
+  object-fit: cover;
+`;
+
+export default class BigPicture extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { url } = props;
+
+    this.state = {
+      url,
+    };
+  }
+
+  render() {
+    const { url } = this.state;
+    return (
+      <BigPictureFrame>
+        <Img src={url} />
+      </BigPictureFrame>
+    );
+  }
 }
+
+BigPicture.propTypes = {
+  url: PropTypes.string.isRequired,
+};
